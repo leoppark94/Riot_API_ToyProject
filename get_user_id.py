34564c862842e956f2fc757fe221d42d161042e1
leoppark94 <65339488+leoppark94@users.noreply.github.com>
2021-07-30 17:31:16 +0900

    1.1.1
    회사에서 수정중

--- get_user_id.py
+++ get_user_id.py
@@ -2,11 +2,16 @@ import requests
 
 def get_user_id(summoner_name, key):
     url = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summoner_name + "?api_key=" + key
-
+    
     result = requests.get(url)
-
+    
+    print("결과")
+    print(result.json())
+    
     encrypted_id = result.json()['id']
+    
     puuid = result.json()['puuid']
 
     return encrypted_id, puuid
+
 
